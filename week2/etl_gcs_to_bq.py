a8dca89d2f3af8dc1922ed3fcbbadd18f458986e
Ahmad Rizky <99194827+ahmdxrzky@users.noreply.github.com>
2023-02-05 16:23:52 +0700

    Update etl_gcs_to_bq.py

--- week2/etl_gcs_to_bq.py
+++ week2/etl_gcs_to_bq.py
@@ -11,8 +11,10 @@ def extract_from_gcs(color: str, year: int, month: int) -> Path:
     """Download trip data from GCS"""
     gcs_path = f"data/{color}/{color}_tripdata_{year}-{month:02}.parquet"
     gcs_block = GcsBucket.load("gcs-bucket")
+    # edit starts here
     gcs_block.get_directory(from_path = gcs_path, local_path = os.path.join("data", "download"))
     return Path(os.path.join("data", "download", gcs_path))
+    # edit ends here
 
 @task()
 def write_bq(df: pd.DataFrame) -> int:
@@ -20,14 +22,18 @@ def write_bq(df: pd.DataFrame) -> int:
     gcp_credentials = GcpCredentials.load("gcp-creds")
 
     df.to_gbq(
+        # edit starts here
         destination_table="dezoomcamp.yellow_rides",
         project_id="de-zoomcamp-375916",
+        # edit ends here
         credentials=gcp_credentials.get_credentials_from_service_account(),
         chunksize=500_000,
         if_exists="append"
     )
 
+    # edit starts here
     return len(df)
+    # edit ends here
 
 @flow()
 def etl_gcs_to_bq(
@@ -39,13 +45,19 @@ def etl_gcs_to_bq(
     path = extract_from_gcs(color, year, month)
     df = pd.read_parquet(path)
     total = write_bq(df)
+    # edit starts here
     return total
+    # edit ends here
 
+# edit starts here
 @flow(log_prints=True)
+# edit ends here
 def etl_main_flow(
+    # edit starts here
     color: str = "yellow",
-    year: int = "2019",
+    year: int = 2019,
     months: list = [2, 3]
+    # edit ends here
 ) -> None:
     """Main ETL function"""
     total_rows = 0
