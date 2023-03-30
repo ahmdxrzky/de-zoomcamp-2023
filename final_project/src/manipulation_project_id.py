a27926f6476173a15cb6394adbf203ba5c862f7b
ahmdxrzky <rizky@skinmystery.com>
2023-03-31 06:40:31 +0700

    Upload to docker hub

--- final_project/src/manipulation_project_id.py
+++ final_project/src/manipulation_project_id.py
@@ -2,16 +2,14 @@ import os
 import sys
 from pathlib import Path
 
-arg1 = sys.argv[1]
 pardir = Path(__file__).absolute().parent.parent
-files = [os.path.join(pardir, "Dockerfile"), os.path.join(pardir, "terraform", "variables.tf")]
 
-for filename in files:
-    with open(filename, "r+") as f:
-        contents = f.read()
-        new_contents = contents.replace("<gcp-project-id>", str(arg1))
-        f.seek(0)
-        f.write(new_contents)
-        f.close()
+arg1 = sys.argv[1]
+with open(os.path.join(pardir, "terraform", "variables.tf"), "r+") as f:
+    contents = f.read()
+    new_contents = contents.replace("<gcp-project-id>", str(arg1))
+    f.seek(0)
+    f.write(new_contents)
+    f.close()
 
-print("Project ID has been ingested two those files.")
+print("Project ID has been ingested.")
