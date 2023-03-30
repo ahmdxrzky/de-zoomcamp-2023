2e0e487f20083d5d90ea0c4a15df7af76ebd74dd
ahmdxrzky <rizky@skinmystery.com>
2023-03-30 07:41:07 +0700

    location dataset

--- final_project/terraform/variables.tf
+++ final_project/terraform/variables.tf
@@ -9,7 +9,7 @@ variable "project" {
 
 variable "region" {
   description = "Region for GCP resources. Choose as per your location: https://cloud.google.com/about/locations"
-  default = "asia-southeast2"
+  default = "US"
   type = string
 }
 
